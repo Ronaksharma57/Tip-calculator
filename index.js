2e94c96169295230b7011b3ae88272dced4c7505
Ronaksharma57 <sharmaronak57@gmail.com>
2023-08-30 15:51:12 +0530

    tip calculator

---
+++ index.js
@@ -0,0 +1,15 @@
+let container = document.querySelector(".area");
+let calculate = document.querySelector("#calculate");
+let div = document.createElement("p");
+calculate.addEventListener("click",()=>{
+    div.innerText =""
+    let amount=document.querySelector('#billAmount').value;
+    let totalPeople = document.getElementById("totalPeople").value;
+  
+    var service = document.getElementById("ddlViewBy").value;
+    div.innerText = ` Tip Amount is ${amount*service/totalPeople}`
+    // console.log(div);
+    container.appendChild(div);
+
+   
+})
